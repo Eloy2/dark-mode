d71586804ca074b121f25c61779d7ecaea2ff2c3
Eloy <eloydgutierrez@gmail.com>
2020-04-14 15:02:38 -0400

    added code to meet MVP

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = () => {
+    const [someValue, setSomeValue] = useLocalStorage("darkMode", false);
+
+    useEffect(() => {
+        if (someValue === true) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    }, [someValue]);
+
+    return [someValue, setSomeValue];
+}
+
+export default useDarkMode;
